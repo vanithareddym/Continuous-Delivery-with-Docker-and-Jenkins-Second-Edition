417c2b1690e4acd35335802a5e2658aa23d4a5de
Vanitha <41925556+vanithareddym@users.noreply.github.com>
2021-10-21 20:45:22 -0400

    Update Calculator.java

--- Chapter09/sample3/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter09/sample3/src/main/java/com/leszko/calculator/Calculator.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Service;
 /** Calculator logic */
 @Service
 public class Calculator {
-	@Cacheable("sum")
+	@Cacheable("div")
 	public int sum(int a, int b) {
 		return a + b;
 	}
 
-        @Cacheable("div")
+        @Cacheable("sum")
         public int div(int a, int b) {
                 return a / b;
         }
